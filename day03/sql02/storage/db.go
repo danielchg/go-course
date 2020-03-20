310f57852770d6eb1abbcf6a2600c6dedf39e5a8
Daniel Clowes <daniel.clowes@intelygenz.com>
2020-03-20 11:43:50 +0100

    implement create user method

--- day03/sql02/storage/db.go
+++ day03/sql02/storage/db.go
@@ -6,8 +6,14 @@ type mySQL struct {
 	db *sql.DB
 }
 
+func NewMySQL(db *sql.DB) Storage {
+	return &mySQL{db: db}
+}
+
 func (m *mySQL) CreateUser(u *User) error {
-	return nil
+	const q = `insert into users values (?,?,?)`
+	_, err := m.db.Exec(q, u.ID, u.Name, u.Age)
+	return err
 }
 
 func (m *mySQL) UpdateUser(u *User) error {
@@ -19,5 +25,5 @@ func (m *mySQL) DeleteUser(id int) error {
 }
 
 func (m *mySQL) GetUser(id int) (User, error) {
-	return nil
+	return User{}, nil
 }
